97a3b4bd278f991d1faede15decc6f6a378823db
celoazevedo <meazevedo86@gmail.com>
2020-04-21 16:12:10 -0700

    finishing template literals challenges. Need to complete #05

--- challenges/02-template-literals/05-stringWithObject.js
+++ challenges/02-template-literals/05-stringWithObject.js
@@ -3,9 +3,11 @@
 //Make use of interpolation to include the property from the object at the requested array index in place of the ____.
 
 const stringWithObject = (array, index, key) => {
-    
+
+    return `The information you requested is ${array.index}. Have a nice day!`
 };
 
+//include the property from the object at the requested array index
 
 
 
